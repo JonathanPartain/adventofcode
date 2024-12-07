a6184b4e280b8238eceaa18192b9cb6fd2fcbfe4
JonathanPartain <jonathan.partain@protonmail.com>
2024-12-07 12:39:39 +0100

    day 7, 600ms at release compilation

--- 2024/rust/day07/src/main.rs
+++ 2024/rust/day07/src/main.rs
@@ -1,66 +1,99 @@
-use std::fmt::write;
+use std::time::Instant;
 
 fn main() {
-    let input = include_str!("../small.txt");
+    let start = Instant::now();
+    let input = include_str!("../input.txt");
     let mut input_list: Vec<&str> = input.split("\n").collect();
     // remove last index, as it is just an empty str
     input_list.remove(input_list.len() - 1);
 
-    let num_list: Vec<Vec<u32>> = input_list
+    let num_list: Vec<Vec<u64>> = input_list
         .iter()
         .map(|item| {
             item.split(" ")
-                .map(|s| s.replace(":", "").parse::<u32>().unwrap())
+                .map(|s| s.replace(":", "").parse::<u64>().unwrap())
                 .collect()
         })
         .collect();
 
+    let mut p1_sum = 0;
+    let mut p2_sum = 0;
     for i in num_list.iter() {
-        do_math(&i.to_vec());
+        let r = do_math(&i.to_vec());
+        p1_sum += r.0;
+        p2_sum += r.1;
     }
+
+    println!("Part 1: {}", p1_sum);
+    println!("Part 2: {}", p2_sum);
+    println!("Exection time: {:.3?}", start.elapsed());
 }
 
-fn add(x: &u32, y: &u32) -> u32 {
+fn add(x: &u64, y: &u64) -> u64 {
     x + y
 }
-fn mul(x: &u32, y: &u32) -> u32 {
+fn mul(x: &u64, y: &u64) -> u64 {
     x * y
 }
-/*
-fn do_op(x, y, op) {
-    ret
+
+fn cat(x: &u64, y: &u64) -> u64 {
+    let sx = x.to_string();
+    let sy = y.to_string();
+    (sx + &sy).parse::<u64>().unwrap()
 }
-*/
-/* Recursive function.
- * Take 2 numbers, do all possible math.
- * For each result, take next number, do all possible math
- * end up with a list/struct whatever of different results. if any are equal
- * to first num, success!
- */
 
-fn do_math(numbers: &Vec<u32>) {
-    let res = numbers[0];
+fn do_ops(x: &u64, y: &u64) -> Vec<u64> {
+    let ops = vec![add, mul];
+    let mut ret_vec: Vec<u64> = vec![];
+    for op in ops {
+        let res = op(&x, &y);
+        ret_vec.push(res);
+    }
+    return ret_vec;
+}
+fn do_ops_cat(x: &u64, y: &u64) -> Vec<u64> {
+    let ops = vec![add, mul, cat];
+    let mut ret_vec: Vec<u64> = vec![];
+    for op in ops {
+        let res = op(&x, &y);
+        ret_vec.push(res);
+    }
+    return ret_vec;
+}
 
-    let mut total = numbers[1];
-    let operations = vec![add, mul];
+fn vec_multi(prev_res: &Vec<u64>, num: &u64, func: fn(&u64, &u64) -> Vec<u64>) -> Vec<u64> {
+    let mut ret: Vec<u64> = vec![];
+    for n in prev_res.iter() {
+        ret.extend(func(&n, &num));
+    }
+    return ret;
+}
+
+fn do_math(numbers: &Vec<u64>) -> (u64, u64) {
+    let mut p1sum = 0;
+    let mut p2sum = 0;
+    let should_equal = numbers[0];
+    let first = numbers[1];
+
+    let mut p1: Vec<u64> = vec![];
+    p1.push(first);
+
+    let mut p2: Vec<u64> = vec![];
+    p2.push(first);
 
     for (pos_n, n) in numbers.iter().enumerate() {
-        let mut tmp = 0;
-        for (pos_m, m) in numbers.iter().enumerate() {
-            if pos_n == 0 {
-                continue;
-            }
-            // more right only
-            if pos_m <= pos_n {
-                continue;
-            }
-
-            for op in &operations {
-                let last_op = op(n, m) + tmp;
-                if last_op == res {
-                    println!("Done!, {}", last_op);
-                }
-            }
+        if pos_n > 1 {
+            let p1_replacer = vec_multi(&p1, &n, do_ops);
+            p1 = p1_replacer;
+            let p2_replacer = vec_multi(&p2, &n, do_ops_cat);
+            p2 = p2_replacer;
         }
     }
+    if p1.contains(&should_equal) {
+        p1sum += should_equal;
+    }
+    if p2.contains(&should_equal) {
+        p2sum += should_equal;
+    }
+    (p1sum, p2sum)
 }
