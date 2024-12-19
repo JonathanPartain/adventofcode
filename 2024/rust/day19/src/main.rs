12c89f7f72b0df638a9d2d137f9f4a849c1454c4
JonathanPartain <jonathan.partain@protonmail.com>
2024-12-19 11:58:41 +0100

    added a counter to memoization

--- 2024/rust/day19/src/main.rs
+++ 2024/rust/day19/src/main.rs
@@ -18,33 +18,44 @@ fn main() {
 //          return solution(target[x..], frags)
 // return false?
 
-fn can_form_word(target: &str, frags: &Vec<&str>, memo: &mut HashMap<String, bool>) -> bool {
+fn can_form_word(
+    target: &str,
+    frags: &Vec<&str>,
+    memo: &mut HashMap<String, (bool, usize)>,
+) -> (bool, usize) {
     if target == "" {
-        return true;
+        return (true, 1);
     }
     if let Some(&cached) = memo.get(target) {
         return cached; // Return cached result
     }
+    let mut total_count = 0;
+    let mut can_form = false;
+
     for frag in frags.iter() {
         let x = frag.len();
         if target.len() >= x && target[..x] == **frag {
-            if can_form_word(&target[x..], frags, memo) {
-                memo.insert(target.to_string(), true);
-                return true;
+            let (sub_can_form, sub_count) = can_form_word(&target[x..], frags, memo);
+            if sub_can_form {
+                can_form = true;
             }
+            total_count += sub_count;
         }
     }
-    memo.insert(target.to_string(), false);
-    return false;
+    memo.insert(target.to_string(), (can_form, total_count));
+    return (can_form, total_count);
 }
 fn part_one(frags: &Vec<&str>, patterns: &str) {
     let mut count = 0;
     let mut memo = HashMap::new();
+    let mut tot = 0;
     for p in patterns.lines() {
-        if can_form_word(p, frags, &mut memo) {
-            println!("{} can be formed", p);
+        let (can_be_formed, c) = can_form_word(p, frags, &mut memo);
+        if can_be_formed {
+            tot += c;
             count += 1;
         }
     }
-    println!("number of word formed: {}", count);
+    println!("Part 1: {}", count);
+    println!("Part 2: {:?}", tot);
 }
